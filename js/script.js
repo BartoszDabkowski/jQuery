209c169790ce4b038ee2af951e232a106ecdec38
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-07-30 13:52:19 -0700

    Animation -- show, hide, toggle

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 $(function() {
-  // jQuery goes here...
 
-  // Uncomment this line to fade out the red box on page load
-    $(".red-box").fadeOut(2000);
-    $(".red-box").fadeIn(1000);
-    $(".red-box").fadeTo(1000, 0.5);
-    $(".green-box").fadeOut(4000);
-    $(".blue-box").fadeTo(4000, 0.25);
+    $("#red-box").hide(1000);
+    $("#green-box").hide(1000);
+    $("#blue-box").hide(1000);
 
-    $(".blue-box").fadeToggle(1000);
-    $(".red-box").fadeIn(1000);
-    $(".blue-box").fadeIn(1000);
-    $(".blue-box").fadeTo(1000, 1.0);
-    $(".green-box").fadeIn(1000);
-    $(".green-box").fadeTo(1000, 0);
+    $("#red-box").show(1000);
+    $("#green-box").show(2000);
+    $("#blue-box").show(3000);
+
+    $("#blue-box").toggle(2000);
+    $("#blue-box").toggle(2000);
 
 });
