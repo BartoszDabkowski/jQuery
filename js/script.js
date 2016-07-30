f341f5c9ad07f0c20d7ae6236a8656a09b0ddfad
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-07-30 14:35:23 -0700

    Animation -- delay, (Chaining Concept)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 $(function() {
-    $("#blue-box").animate({
-        "margin-left": "+=200px",
-        "margin-top": "25px",
-        "opacity": "0",
-        "height": "50px",
-        "width": "50px"
-    }, 1000);
-
-    $("#blue-box").animate({
-        "margin-left": "-=200px",
-        "opacity": "0.75"
-    }, 250);
-
-    $("#red-box").animate({
-        "margin-top": "-=125px"
-    }, 250);
-
-    $("p").animate({
-        "font-size": "20px"
-    }, 1000);
+    $("#red-box").fadeTo(1000, .25).delay(3000).fadeTo(1000, 1.0);
+    $("#green-box").delay(1000).fadeTo(1000, .25);
+    $("#blue-box").delay(2000).fadeTo(1000, .25);
 });
