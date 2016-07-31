fe5cfd1cf3e9445b4c9ae59d01de6c1dff962ab5
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-07-30 18:24:06 -0700

    Adding HTML DOM Elements -- append, prepend, after, before

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
 $(function() {
-    $("#list1").children("li").addClass("highlight1");
-    $("#list2").find("li").addClass("highlight2");
-    $("#list3").find("ul").addClass("highlight3");
-    $("#list4").find("ul").addClass("highlight4");
-
-    $("#list4")
-        .children("li")
-        .children("ul")
-        .children("li")
-        .filter(function(index){
-            return index % 3 === 0;
-        })
-        .css("background-color", "rgba(255, 150, 200, 1)");
-
     $("#list1")
-        .children("li")
-        .not(":first")
-        .not(":last")
-        .css("background-color", "rgba(255, 150, 200, 1)");
+        .append("<li>Main Item 5" +
+                "<ul>" +
+                "<li>Inserted 1</li>" +
+                "</i>Inserted 2</li>" +
+                "</ul>" +
+                "</li>");
+
+    $("#list1").children("li:last").addClass("highlight1");
+
+    $("#red-box").after("<div id='purple-box'>Purple</div>");
+    $("#purple-box").before("<div id='orange-box'>Orange</div>");
+
+    $("#blue-box").after($("#red-box"));
 });
