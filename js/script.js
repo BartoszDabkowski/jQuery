42eda500be52d5c2e5b4e6a37b48dfa2f4ca23c5
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-08-01 16:28:06 -0700

    Events

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 $(function () {
-    var par = $("p");
-    console.log(par.text());
-    console.log(par.html());
+    $('#button').on("click", function(e){
+        console.log(e);
+    });
 
-    par.text("<strong>Hello</strong>");
-    par.html("<strong>Hello</strong>");
-    par.html(par.html() + " hello");
+    $('#red-box').on({
+        mouseenter: function(){
+            $(this).stop().fadeTo(1000, .5);
+        },
+        mouseleave: function(){
+            $(this).stop().fadeTo(1000, 1);
+        }
+    });
+
+    $('#content').on("click",'p', function(){
+        $(this).slideUp();
+    });
+
+    $('#pars').append("<p>dynamically allocated</p>");
+
+    $('#button').click({
+        user: "Peter",
+        num: 10
+    },function(event){
+        alert(event.data.user + " is " + event.data.num);
+    });
+
+    $('html').keydown(function(event){
+        if(event.which === 39){
+            $('#blue-box').stop().animate({
+                marginLeft: '+=10px'
+            }, 50)
+        }
+    });
 });
-
